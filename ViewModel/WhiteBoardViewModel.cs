3de423cf94f76666ba4ec9b261e44700a3f55385
Pierre Feytout <pierre.feytout@epitech.eu>
2015-07-14 23:29:56 +0200

    WhiteBoard Commit
    WhiteBoard with line, rectangle, circle.
    MVVM pattern not implemented for whiteboard due to technical
    difficulties.

---
+++ ViewModel/WhiteBoardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.ViewModel
+{
+    class WhiteBoardViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
