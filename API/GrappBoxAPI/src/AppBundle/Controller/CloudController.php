0f6c6d70947d94f3be05032df1153ebfb656a7be
Marc Wieser <marc.wieser@epitech.eu>
2015-09-18 17:27:24 +0900

    Manage errors on opening stream with cloud

--- API/GrappBoxAPI/src/AppBundle/Controller/CloudController.php
+++ API/GrappBoxAPI/src/AppBundle/Controller/CloudController.php
@@ -3,9 +3,21 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CloudController extends Controller
 {
+    private $POSTRequestMethodError = new JsonResponse()->setData(array(
+      "success" => false,
+      "infos" => "This have to be a POST request"
+    ));
+
+    private $authorizationError = new JsonResponse()->setData(array(
+      "success" => false,
+      "infos" => "You haven't the authorization to have this information"
+    ));
+
     private function checkTokenAuthorization($token, $idProject)
     {
         //Check if the person behind the $token have the authorization to use
@@ -14,38 +26,48 @@ class CloudController extends Controller
     }
 
     //This have to be a POST request
+    /* Requested json
+    {
+      token : "userToken",
+      project_id : 42,
+      filename : "Awesomeness"
+    }
+    */
     public function openStreamAction(Request $request)
     {
+        if ($request->getMethod() != "POST")
+          return $this->POSTRequestMethodError;
+        $dbManager = $this->getDoctrine()->getManager();
+        $token = $request->get("token");
+        $idProject = $request->get("project_id");
         if (!$this->checkTokenAuthorization($token, $idProject))
-        {
-          // If the user haven't authorization, send error json
-        }
+          return $this->authorizationError;
     }
 
     //This have to be a POST request
     public function closeStreamAction(Request $request)
     {
-        if (!$this->checkTokenAuthorization($token, $idProject))
-        {
-          // If the user haven't authorization, send error message
-        }
+        if ($request->getMethod() != "POST")
+          return $this->POSTRequestMethodError;
+        //Check if the token in the request have an id_project transfer launched
     }
 
+    //This have to be a POST request
     public function sendFileAction(Request $request)
     {
+        if ($request->getMethod() != "POST")
+          return $this->POSTRequestMethodError;
         if (!$this->checkTokenAuthorization($token, $idProject))
-        {
-          // If the user haven't authorization, send error message
-        }
+          return $this->authorizationError;
     }
 
-    //This have to be a GET request
+    //This have to be a POST request
     public function listFilesAndDirectoriesAction(Request $request)
     {
+        if ($request->getMethod() != "POST")
+          return $this->POSTRequestMethodError;
         if (!$this->checkTokenAuthorization($token, $idProject))
-        {
-          // If the user haven't authorization, send error message
-        }
+          return $this->authorizationError;
     }
 
 }
