14ce2ad1544b3e1002eadf00ed75055fbde2a88c
hofman_p <pierre.hofman@epitech.eu>
2015-09-19 20:15:56 +0900

    Removed Ratchet. Added Angular. Started Dashboard with a side menu with Angular. Angular routing with templates is coming soon.

---
+++ IOS/www/scripts/controllers.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('GrappBox', ['ionic'])
+
+app.controller('MainCtrl', function ($scope, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft()
+    }
+})
