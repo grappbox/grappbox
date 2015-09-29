c6ee20c61da975cb42f5d1b59deb57321e707044
feytou-p <pierre.feytout@epitech.eu>
2015-09-28 20:21:06 -0400

    DashBoard Interface with MVVM pattern, base of  api communication, and navigation through pages. The interface is oriented around a Pivot style application. https://msdn.microsoft.com/en-us/library/windows/apps/hh202892%28v=vs.105%29.aspx

---
+++ MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace GrappBox
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Constructor
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+
+        /// <summary>
+        /// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
+        /// </summary>
+        /// <param name="e">Données d’événement décrivant la manière dont l’utilisateur a accédé à cette page.
+        /// Ce paramètre est généralement utilisé pour configurer la page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void DashBoardButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GrappBox.View.DashBoardView));
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Microsoft.Xaml.Interactions.Core;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.Foundation;
+using System.Diagnostics;
+
+namespace GrappBox.ViewModel
+{
+    public class TappedPositionConverter : IValueConverter
+    {
+        public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        string language)
+        {
+            var args = (TappedRoutedEventArgs)value;
+            var element = (FrameworkElement)parameter;
+
+            var point = args.GetPosition(element);
+            return new Point(point.X, point.Y);
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CommandHandler : ICommand
+    {
+        public Action _action;
+        public bool _canExecute;
+        public event EventHandler CanExecuteChanged;
+        public CommandHandler(Action action)
+        {
+            _action = action;
+            _canExecute = false;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_action == null)
+                return false;
+            else
+                return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+    public class CommandHandler<T> : ICommand
+    {
+        public Action<T> _action;
+        public bool _canExecute;
+        public event EventHandler CanExecuteChanged;
+        public CommandHandler(Action<T> action)
+        {
+            _action = action;
+            _canExecute = false;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_action == null)
+                return false;
+            else
+                return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            T param = (T)parameter;
+            _action.Invoke(param);
+        }
+    }
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string property)
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+                Debug.WriteLine("Notify " + property);
+            }
+
+        }
+    }
+}
