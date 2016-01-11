d8e65c56042f2b91c5e9f7f1a870c1c57eff9255
mougen_v <valentin.mougenot@epitech.eu>
2016-01-11 15:05:25 +0800

    remove AppBundle + nelmioApiDoc + update composer

--- API/GrappBoxAPI/app/AppKernel.php
+++ API/GrappBoxAPI/app/AppKernel.php
@@ -16,9 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new APIBundle\APIBundle(),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new GrappboxBundle\GrappboxBundle(),
         );
