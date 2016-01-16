908a374a2ec5d225f9011b8df930d417b90c7fd2
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-01-16 01:38:15 +0100

    Update dashboard data management. - Update API connectivity fails handles. - Add user's next meetings date format. - Update styles for next meetings.

--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/dashboard/app-dashboard.angular.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/dashboard/app-dashboard.angular.js
@@ -10,15 +10,37 @@
 *
 */
 app.controller('dashboardController', ['$rootScope', '$scope', '$http', '$cookies', function($rootScope, $scope, $http, $cookies) {
-	$http.get($rootScope.apiBaseURL + '/dashboard/getteamoccupation/' + $cookies.get('USERTOKEN')).success(function(data) {
-		$scope.teamOccupationList = data;
-	});
+	// Get current team occupation
+	$http.get($rootScope.apiBaseURL + '/dashboard/getteamoccupation/' + $cookies.get('USERTOKEN'))
+		.then(function successCallback(response) {
+			$scope.teamOccupationList = (response.data && Object.keys(response.data).length ? response.data : null);
+			$scope.teamOccupationList_isValid = true;
+		},
+		function errorCallback(response) {
+			$scope.teamOccupationList = null;
+			$scope.teamOccupationList_isValid = false;
+		});
 
-	$http.get($rootScope.apiBaseURL + '/dashboard/getnextmeetings/' + $cookies.get('USERTOKEN')).success(function(data) {
-		$scope.nextMeetingsList = data;
-	});
+	// Get user's next mettings
+	$http.get($rootScope.apiBaseURL + '/dashboard/getnextmeetings/' + $cookies.get('USERTOKEN'))
+		.then(function successCallback(response) {
+			$scope.nextMeetingsList = (response.data && Object.keys(response.data).length ? response.data : null);
+			$scope.nextMeetingsList_isValid = true;
+			$scope.formatDate = function(dateToFormat) { return dateToFormat.substring(0, dateToFormat.lastIndexOf(':')); }
+		},
+		function errorCallback(response) {
+			$scope.nextMeetingsList = null;
+			$scope.nextMeetingsList_isValid = false;
+		});
 
-	$http.get($rootScope.apiBaseURL + '/dashboard/getprojectsglobalprogress/' + $cookies.get('USERTOKEN')).success(function(data) {
-		$scope.globalProgressList = data;
-	});
+	// Get user's current projects (and progress)
+	$http.get($rootScope.apiBaseURL + '/dashboard/getprojectsglobalprogress/' + $cookies.get('USERTOKEN'))
+		.then(function successCallback(response) {
+			$scope.globalProgressList = (response.data && Object.keys(response.data).length ? response.data : null);
+			$scope.globalProgressList_isValid = true;
+		},
+		function errorCallback(response) {
+			$scope.globalProgressList = null;
+			$scope.globalProgressList_isValid = false;
+		});
 }]);
