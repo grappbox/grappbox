b2fdc2be4274bafa5b28e4ce48fa15ee91bab18e
launoi_a <launoi_a@epitech.eu>
2016-01-25 21:00:41 +0900

    start implementation of bugtracker list page in static

--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/bugtracker/bugtracker-list.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/bugtracker/bugtracker-list.js
@@ -9,11 +9,26 @@
 * APP bugtracker page list (one per project)
 *
 */
-app.controller('bugtrackerListController', ['$scope', function($scope) {
+app.controller('bugtrackerListController', ['$scope', '$routeParams, $http', function($scope, $routeParams, $http) {
   $scope.alertList = [];
+
+  $scope.closeAlert = function(index) {
+    $scope.alertList.splice(index, 1);
+  };
+
   $scope.alertList.push({
     type: 'warning',
     message: 'This section is under construction.'
+  });
+
+  if ($routeParams.id)
+    $scope.alertList.push({
+      type: 'danger',
+      message: 'Ticket #' + $routeParams.id + 'doesn\'t exist, or you might not have the rights to see it. Please try again.'
+    });
+
+  $http.get('../resources/_temp/bugtraker.json').success(function(data) {
+    $scope.bugtrackerListContent = data;
   });
 }]);
 
@@ -23,11 +38,30 @@ app.controller('bugtrackerListController', ['$scope', function($scope) {
 * Check if requested bugtracker is accessible
 *
 */
-var bugtracker_isAccessible = function($q) {
+var bugtracker_isAccessible = function($http, $route, $q, $location) {
+  var bugtrackerListContent = "";
+  var isAccessible = false;
   var deferred = $q.defer();
-  deferred.resolve(true);
 
-  return deferred.promise;
+  $http.get('../ressources/_temp/bugtracker.json').success(function(data) {
+    bugtrackerListContent = angular.fromJson(data);
+
+    for(i = 0; i < bugtrackerListContent.length; ++i) {
+      if (bugtrackerListContent[i].id == $route.current.params.id)
+        isAccessible = true;
+    }
+
+    if (isAccessible)
+      deferred.resolve(true);
+    else {
+      deferred.reject();
+      $location.path('bugtracker').search({
+        'id': route.current.params.id
+      });
+    }
+
+    return deferred.promise;
+  });
 };
 
-bugtracker_isAccessible['$inject'] = ['$q'];
+bugtracker_isAccessible['$inject'] = ['$http', '$route', '$q', '$location'];
