563fc3596a2a0a30b4d61069d78328129853d2f4
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-02-03 14:45:27 +0000

    Update all statements naming convention. - Replace single quotes with double quotes as main symbol for definitions, declarations, and attributes - Update AppController/app[.css;.js] and cloud/cloud-list[.html;.js] with new statement convention - Fix _prod compilation on Cloud pages (due to bad statements)

--- Web/Work/src/GrappBox/Bundle/AppBundle/Controller/AppController.php
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Controller/AppController.php
@@ -2,7 +2,7 @@
 
 /*!
 * This file is subject to the terms and conditions defined in
-* file 'LICENSE.txt', which is part of the GRAPPBOX source code package.
+* file "LICENSE.txt", which is part of the GRAPPBOX source code package.
 * COPYRIGHT GRAPPBOX. ALL RIGHTS RESERVED.
 */
 
@@ -19,21 +19,22 @@ class AppController extends Controller
 	{
 		$pathData = $request->getpathInfo();
 
-		if (($token = strpos($pathData, '/app/')) !== false)
-			return $this->redirect(substr_replace($pathData, '/app/#/', $token, strlen('/app/')), 301);
+		if (($token = strpos($pathData, "/app/")) !== false)
+			return $this->redirect(substr_replace($pathData, "/app/#/", $token, strlen("/app/")), 301);
 	}
 
 	public function indexAction()
 	{
-		$requestData = $this->get('request');
+		$requestData = $this->get("request");
 		$cookieData = $requestData->cookies;
 
-		if ($cookieData->has('USERTOKEN') && $cookieData->get('USERTOKEN'))
-			return $this->render('AppBundle:App:index.html.twig');
+		if ($cookieData->has("USERTOKEN") && $cookieData->get("USERTOKEN"))
+			return $this->render("AppBundle:App:index.html.twig");
 
-		$redirectResponse = new RedirectResponse('/#login');
-		$redirectResponse->headers->setCookie(new Cookie('LASTLOGINMESSAGE', hash('sha512', '_DENIED'), 0, '/', null, false, false));
-		$redirectResponse->headers->setCookie(new Cookie('USERTOKEN', null, 0, '/', null, false, false));
+		$redirectResponse = new RedirectResponse("/#login");
+		$redirectResponse->headers->setCookie(new Cookie("LASTLOGINMESSAGE", hash("sha512", "_DENIED"), 0, "/", null, false, false));
+		$redirectResponse->headers->setCookie(new Cookie("USERTOKEN", null, 0, "/", null, false, false));
+		$redirectResponse->headers->setCookie(new Cookie("CLOUDSAFE", null, 0, "/", null, false, false));
 
 		return $redirectResponse;
 	}
--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/_app.angular.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/_app.angular.js
@@ -1,85 +1,101 @@
 /*
-* This file is subject to the terms and conditions defined in
-* file 'LICENSE.txt', which is part of the GRAPPBOX source code package.
-* COPYRIGHT GRAPPBOX. ALL RIGHTS RESERVED.
-*/
+ * This file is subject to the terms and conditions defined in
+ * file "LICENSE.txt", which is part of the GRAPPBOX source code package.
+ * COPYRIGHT GRAPPBOX. ALL RIGHTS RESERVED.
+ */
 
 /**
-* GRAPPBOX
-* APP and main controller definition
-*
-*/
-var app = angular.module('grappbox', ['ngRoute', 'ngCookies', 'ngAnimate', 'ui.bootstrap', 'panhandler', 'ui-notification', 'naif.base64']);
+ * GRAPPBOX
+ * APP and main controller definition
+ *
+ */
+var app = angular.module("grappbox", ["ngRoute", "ngCookies", "ngAnimate", "ui.bootstrap", "panhandler", "ui-notification", "naif.base64"]);
 
 // TWIG template conflict fix
-app.config(['$interpolateProvider', function($interpolateProvider) {
-	$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+app.config(["$interpolateProvider", function($interpolateProvider) {
+	$interpolateProvider.startSymbol("{[{").endSymbol("}]}");
 }]);
 
 // Cross-domain URLs calls fix
-app.config(['$httpProvider', function($httpProvider) {
+app.config(["$httpProvider", function($httpProvider) {
 	$httpProvider.defaults.useXDomain = true;
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	delete $httpProvider.defaults.headers.common["X-Requested-With"];
 }]);
 
 // Bootstrap notifications settings
-app.config(['NotificationProvider', function(NotificationProvider) {
+app.config(["NotificationProvider", function(NotificationProvider) {
 	NotificationProvider.setOptions({
-		delay: 10000,
 		startTop: 20,
 		startRight: 10,
 		verticalSpacing: 20,
 		horizontalSpacing: 20,
-		positionX: 'right',
-		positionY: 'top'
+		positionX: "right",
+		positionY: "top"
 	});
 }]);
 
 // Main controller definition
-app.controller('grappboxController', ['$scope', '$location', function($scope, $location) {
+app.controller("grappboxController", ["$scope", "$location", function($scope, $location) {
 	$scope.isPageActive = function(route) {
 		var isPageActive = false;
-		switch(route) {
-			case '/whiteboard':
-				isPageActive = ($location.path().indexOf('whiteboard') > -1);
+		switch (route) {
+			case "/cloud":
+				isPageActive = ($location.path().indexOf("cloud") > -1);
 				break;
-			case '/cloud':
-				isPageActive = ($location.path().indexOf('cloud') > -1);
+			case "/bugtracker":
+				isPageActive = ($location.path().indexOf("bugtracker") > -1);
+				break;
+			case "/whiteboard":
+				isPageActive = ($location.path().indexOf("whiteboard") > -1);
 				break;
 			default:
 				isPageActive = (route === $location.path());
 				break;
-			};
+		};
 		return isPageActive;
 	}
 }]);
 
 
 /**
-* ROOTSCOPE definition
-* 'layout, loading, apiVersion, apiBaseURL'
-*
-*/
-app.run(['$rootScope', '$location', '$cookies', '$http', '$window', function($rootScope, $location, $cookies, $http, $window) {
+ * ROOTSCOPE definition
+ * "layout, loading, apiVersion, apiBaseURL"
+ *
+ */
+app.run(["$rootScope", "$location", "$cookies", "$http", "$window", function($rootScope, $location, $cookies, $http, $window) {
+
 	// ROOTSCOPE variables
-	$rootScope.apiVersion = 'V0.2'
-	$rootScope.apiBaseURL = 'http://api.grappbox.com/app_dev.php/' + $rootScope.apiVersion;
+	$rootScope.apiVersion = "V0.2"
+	$rootScope.apiBaseURL = "http://api.grappbox.com/app_dev.php/" + $rootScope.apiVersion;
+
+	// ROOTSCOPE routine definition
+	// Clear cookies and redirect user to login (with error)
+	$rootScope.onUserTokenError = function() {
+		$cookies.put("LASTLOGINMESSAGE", sha512("_DENIED"), {
+			path: "/"
+		});
+		$cookies.remove("USERTOKEN", {
+			path: "/"
+		});
+		$cookies.remove("CLOUDSAFE", {
+			path: "/"
+		});
+		$window.location.href = "/#login";
+	};
 
 	// On route change (start)
-	$rootScope.$on('$routeChangeStart', function() {
-		if (!$cookies.get('LASTLOGINMESSAGE')) {
-			if ($cookies.get('USERTOKEN'))
-				$http.get($rootScope.apiBaseURL + '/accountadministration/logout/' + $cookies.get('USERTOKEN'));
-			$cookies.put('LASTLOGINMESSAGE', sha512('_DENIED'), { path: '/' });
-			$cookies.remove('USERTOKEN', { path: '/' });
-			$cookies.remove('CLOUDSAFE', { path: '/' });			
-			$window.location.href='/#login';
+	$rootScope.$on("$routeChangeStart", function() {
+		if (!$cookies.get("LASTLOGINMESSAGE")) {
+			if ($cookies.get("USERTOKEN"))
+				$http.get($rootScope.apiBaseURL + "/accountadministration/logout/" + $cookies.get("USERTOKEN"));
+			$rootScope.onUserTokenError();
 		}
 	});
 	// On route change (success)
-	$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+	$rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
 		$rootScope.title = current.$$route.title;
-	 });
+	});
 	// On route change (error)
-	$rootScope.$on('$routeChangeError', function() { });
+	$rootScope.$on("$routeChangeError", function() {});
+
 }]);
