229da0eac621e15ed9a211c87d9ae7b4137ba371
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-02-17 22:43:39 +0000

    Update dashboard content view. - Update API calls to version 0.2 - Update API errors management - Update HTML structure for lighter rendering (with latest generic classes) - Add loading animations on refresh - Update global design - Add new APP generic classes (HTML)

--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/dashboard/dashboard.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/dashboard/dashboard.js
@@ -10,39 +10,80 @@
 *
 */
 app.controller('dashboardController', ['$rootScope', '$scope', '$http', '$cookies', function($rootScope, $scope, $http, $cookies) {
+
+	// Scope variables initialization
+	$scope.data = { teamOccupation: {}, nextMeetings: {}, globalProgress: {} };
+
+
+
+	/* ==================== INITIALIZATION (TEAM OCCUPATION) ==================== */
+
+	$scope.data.teamOccupation = { onLoad: true, list: {}, isValid: false };
+
 	// Get current team occupation
 	$http.get($rootScope.apiBaseURL + '/dashboard/getteamoccupation/' + $cookies.get('USERTOKEN'))
-		.then(function successCallback(response) {
-			$scope.teamOccupationList = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
-			$scope.teamOccupationList_isValid = true;
+		.then(function teamOccupationReceived(response) {
+			$scope.data.teamOccupation.onLoad = false;
+			$scope.data.teamOccupation.list = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
+			$scope.data.teamOccupation.isValid = true;
 		},
-		function errorCallback(response) {
-			$scope.teamOccupationList = null;
-			$scope.teamOccupationList_isValid = false;
+		function teamOccupationNotReceived(response) {
+			$scope.data.teamOccupation.onLoad = false;
+			$scope.data.teamOccupation.list = null;
+			$scope.data.teamOccupation.isValid = false;
+
+			if (response.data.info && response.data.info.return_code == "2.1.3")
+				$rootScope.onUserTokenError();
 		});
+
+
+
+	/* ==================== INITIALIZATION (NEXT MEETINGS) ==================== */
+
+	$scope.data.nextMeetings = { onLoad: true, list: {}, isValid: false };
+
+	// Routine definition
+	// Format date
+	$scope.formatObjectDate = function(dateToFormat) {
+		return dateToFormat.substring(0, dateToFormat.lastIndexOf(':'));
+	};
 
 	// Get user's next mettings
 	$http.get($rootScope.apiBaseURL + '/dashboard/getnextmeetings/' + $cookies.get('USERTOKEN'))
-		.then(function successCallback(response) {
-			$scope.nextMeetingsList = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
-			$scope.nextMeetingsList_isValid = true;
-			$scope.formatDate = function(dateToFormat) { return dateToFormat.substring(0, dateToFormat.lastIndexOf(':')); }
+		.then(function nextMeetingsReceived(response) {
+			$scope.data.nextMeetings.onLoad = false;
+			$scope.data.nextMeetings.list = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
+			$scope.data.nextMeetings.isValid = true;
 		},
-		function errorCallback(response) {
-			$scope.nextMeetingsList = null;
-			$scope.nextMeetingsList_isValid = false;
+		function nextMeetingsNotReceived(response) {
+			$scope.data.teamOccupation.onLoad = false;
+			$scope.data.nextMeetings.list = null;
+			$scope.data.nextMeetings.isValid = false;
+
+			if (response.data.info && response.data.info.return_code == "2.2.3")
+				$rootScope.onUserTokenError();	
 		});
+
+
+
+	/* ==================== INITIALIZATION (GLOBAL PROGRESS) ==================== */
+
+	$scope.data.globalProgress = { onLoad: true, list: {}, isValid: false };
 
 	// Get user's current projects (and progress)
 	$http.get($rootScope.apiBaseURL + '/dashboard/getprojectsglobalprogress/' + $cookies.get('USERTOKEN'))
-		.then(function successCallback(response) {
-			$scope.globalProgressList = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
-			$scope.globalProgressList_isValid = true;
+		.then(function globalProgressReceived(response) {
+			$scope.data.globalProgress.onLoad = false;
+			$scope.data.globalProgress.list = (response.data && response.data.data && Object.keys(response.data.data.array).length ? response.data.data.array : null);
+			$scope.data.globalProgress.isValid = true;
 		},
-		function errorCallback(response) {
-			$scope.globalProgressList = null;
-			$scope.globalProgressList_isValid = false;
-		});
-}]);
+		function globalProgressNotReceived(response) {
+			$scope.data.teamOccupation.onLoad = false;
+			$scope.data.globalProgress.list = null;
+			$scope.data.globalProgress.isValid = false;
 
-// API V0.2 UPDATE MISSING
+			if (response.data.info && response.data.info.return_code == "2.3.3")
+				$rootScope.onUserTokenError();
+		});
+
+}]);
