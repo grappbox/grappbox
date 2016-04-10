0ebb5ad76c83d7dac83d699cdda6bb4a402465dc
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-04-10 14:29:46 +0100

    Update calendar content page view (2/4). - Add events/tasks deletion from GrappBox API - Update event list refresh (on mode change - on previous/next) - Update view styles - Add notifications systems and watch handlers

--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/calendar/calendar.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/calendar/calendar.js
@@ -25,7 +25,7 @@ app.config(["calendarConfig", function(calendarConfig) {
 * APP calendar page content
 *
 */
-app.controller('calendarController', ["$scope", "$http", "$rootScope", "$cookies", "moment", function($scope, $http, $rootScope, $cookies, moment) {
+app.controller('calendarController', ["$scope", "Notification", "$http", "$rootScope", "$cookies", "moment", "$uibModal", function($scope, Notification, $http, $rootScope, $cookies, moment, $uibModal) {
 
   /* ==================== INITIALIZATION ==================== */
 
@@ -33,43 +33,39 @@ app.controller('calendarController', ["$scope", "$http", "$rootScope", "$cookies
   $scope.data = { onLoad: true, calendar: "", isValid: true };
   $scope.events = [];
 
-  // Get user's current month planning
-  $http.get($rootScope.apiBaseURL + "/planning/getmonth/" + $cookies.get("USERTOKEN") + "/" + moment().format('YYYY-MM-DD'))
-    .then(function userCalendarReceived(response) {
-      $scope.data.calendar = (response.data && Object.keys(response.data.data).length ? response.data.data.array : null);
-      $scope.data.isValid = true;
-      $scope.data.onLoad = false;
+  $scope.view_calendarTitle = "";
+  $scope.view_calendarMode = 'month';
+  $scope.view_currentDate = new Date();
+  $scope.view_isCellOpen = true;
 
-      console.log($scope.data.calendar);
-      for (var i = 0; i < $scope.data.calendar.events.length; ++i) {      
-        $scope.events.push({
-          id: $scope.data.calendar.events[i].id,
-          projectId: $scope.data.calendar.events[i].projectId,
-          type: 'event',
-          tag: '[ Event ]',
-          title: $scope.data.calendar.events[i].title,
-          description: $scope.data.calendar.events[i].description,
-          startsAt: moment($scope.data.calendar.events[i].beginDate.date).toDate(),
-          endsAt: moment($scope.data.calendar.events[i].endDate.date).toDate(),
-          draggable: false,
-          resizable: false
-        });
-      }
-/*      for (var i = 0; i < $scope.data.calendar.tasks.length; ++i) {      
-        $scope.events.push({
-          id: $scope.data.calendar.tasks[i].id,
-          projectId: $scope.data.calendar.tasks[i].projectId,
-          type: 'task',
-          tag: '[ Task ]',
-          title: $scope.data.calendar.tasks[i].title,
-          description: $scope.data.calendar.tasks[i].description,
-          startsAt: moment($scope.data.calendar.tasks[i].startedAt.date).toDate(),
-          endsAt: ($scope.data.calendar.tasks[i].finishedAt ? moment($scope.data.calendar.tasks[i].finishedAt.date).toDate() : moment().toDate()),
-          dueDate: moment($scope.data.calendar.tasks[i].dueDate.date).toDate(),
-          draggable: false,
-          resizable: false
-        });
-      }*/
+
+
+  /* ==================== START ==================== */
+
+  // Routine definition
+  // Get user's month planning (based on given start date)
+  var getDatePlanning = function(date) {
+    $http.get($rootScope.apiBaseURL + "/planning/getmonth/" + $cookies.get("USERTOKEN") + "/" + date)
+      .then(function userCalendarReceived(response) {
+        $scope.data.calendar = (response.data && Object.keys(response.data.data).length ? response.data.data.array : null);
+        $scope.data.isValid = true;
+        $scope.data.onLoad = false;
+        $scope.events = [];
+
+        for (var i = 0; i < $scope.data.calendar.events.length; ++i) {      
+          $scope.events.push({
+            id: $scope.data.calendar.events[i].id,
+            projectId: $scope.data.calendar.events[i].projectId,
+            type: 'event',
+            tag: '[ Event ]',
+            title: $scope.data.calendar.events[i].title,
+            description: $scope.data.calendar.events[i].description,
+            startsAt: moment($scope.data.calendar.events[i].beginDate.date).toDate(),
+            endsAt: moment($scope.data.calendar.events[i].endDate.date).toDate(),
+            draggable: false,
+            resizable: false
+          });
+        }
     },
     function userCalendarNotReceived(response) {
       $scope.data.calendar = null;
@@ -77,11 +73,55 @@ app.controller('calendarController', ["$scope", "$http", "$rootScope", "$cookies
       $scope.data.onLoad = false;
       $scope.events = [];
     });
+  }
 
-  $scope.calendarTitle = "";
-  $scope.calendarView = 'month';
-  $scope.viewDate = new Date();
-  $scope.isCellOpen = true;
+  // START
+  getDatePlanning(moment().startOf('month').format('YYYY-MM-DD'));
+
+
+
+  /* ==================== REFRESH OBJECT (EVENT/TASK) ==================== */
+
+  // Routine definition
+  // Refresh event list (based on given start date)
+  var local_refreshEventList = function() {
+    Notification.info({ message: "Loading...", delay: 5000 });    
+    getDatePlanning(moment($scope.view_currentDate).startOf('month').format('YYYY-MM-DD'));
+  };
+
+  // "Previous/Today/Next" button handler
+  $scope.view_updateEventList = function() {
+    local_refreshEventList();
+  };
+
+  // Calendar mode (day/week/month/year) change watch
+  $scope.$watch('view_calendarMode', function() {
+    local_refreshEventList();
+  });
+
+
+
+  /* ==================== DELETE OBJECT (EVENT/TASK) ==================== */
+
+  // "Delete" button handler
+  $scope.view_onDeleteEvent = function(event) {
+    var modalInstance_deleteEvent = "";
+
+    modalInstance_deleteEvent = $uibModal.open({ animation: true, templateUrl: "view_deleteEvent.html", controller: "view_deleteEvent" });
+    modalInstance_deleteEvent.result.then(function deletionConfirmed() {
+      $http.delete($rootScope.apiBaseURL + "/event/delevent/" + $cookies.get("USERTOKEN") + "/" + event.id)
+      .then(function deletionSuccess(response) {
+        Notification.success({ message: "Deletion success.", delay: 5000 });
+        getDatePlanning($scope.view_calendarMode == 'year' ? moment().startOf('year').format('YYYY-MM-DD') : moment().startOf('month').format('YYYY-MM-DD'));
+      }, 
+      function deletionFailure(response) { Notification.warning({ message: "Unable to delete the selected event. Please try again.", delay: 5000 }); });
+    },
+    function deletionNotConfirmed() { });
+  };
+
+
+
+  /* ==================== <waiting> ==================== */
 
   $scope.eventClicked = function(event) {
     console.log('Clicked', event);
@@ -89,10 +129,6 @@ app.controller('calendarController', ["$scope", "$http", "$rootScope", "$cookies
 
   $scope.eventEdited = function(event) {
     console.log('Edited', event);
-  };
-
-  $scope.eventDeleted = function(event) {
-    console.log('Deleted', event);
   };
 
   $scope.eventTimesChanged = function(event) {
@@ -105,4 +141,17 @@ app.controller('calendarController', ["$scope", "$http", "$rootScope", "$cookies
     event[field] = !event[field];
   };
 
+}]);
+
+
+
+/**
+* Controller definition (from view)
+* EVENT DELETION => confirmation?
+*
+*/
+app.controller("view_deleteEvent", ["$scope", "modalInputService", "$uibModalInstance", function($scope, modalInputService, $uibModalInstance) {
+
+  $scope.view_deleteEventSuccess = function() { $uibModalInstance.close(); };
+  $scope.view_deleteEventFailure = function() { $uibModalInstance.dismiss(); };
 }]);
