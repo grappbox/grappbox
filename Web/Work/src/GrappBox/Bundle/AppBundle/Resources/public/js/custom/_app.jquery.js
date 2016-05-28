be7023dfe86ca10160ac044e7e21c08450dc2c71
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-05-28 04:02:28 +0200

    Add Bootstrap Material components. - Add Material Design for Bootstrap CSS components - Add AngularJS messages version 1.5.5 (required) - Add Angular-Boostrap-Material injections Remove unused scripts from APP template. Update custom APP page height jQuery adapter, secure it with jQuery injection.

--- Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/_app.jquery.js
+++ Web/Work/src/GrappBox/Bundle/AppBundle/Resources/public/js/custom/_app.jquery.js
@@ -9,26 +9,12 @@
 * APP additional scripts definition
 *
 */
-$(function() {
+(function($) {
 
-	// Auto page height (depending on current content)
+	// Auto page height (depending on page content)
 	$(window).bind("load resize", function() {
-		newWidth = (this.window.innernewWidth > 0) ? this.window.innernewWidth : this.screen.newWidth;
-		offsetTop = 60;
-		offsetBottom = 123;
-
-		if (newWidth < 768) {
-			$('div.navbar-collapse').addClass('collapse');
-			offsetTop = 100;
-		}
-		else
-			$('div.navbar-collapse').removeClass('collapse');
-
-		newHeight = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-		newHeight = newHeight - offsetTop - offsetBottom;
-		if (newHeight < 1)
-			newHeight = 1;
-		if (newHeight > offsetTop)
-			$("#page-wrapper").css("min-height", newHeight + "px");
+		newHeight = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height);
+		$("#page-content-wrapper").css("min-height", (newHeight < 1 ? 1 : newHeight) + "px");
 	});
-});
+	
+})(jQuery);
