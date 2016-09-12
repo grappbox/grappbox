5f26a163983fb800e55f55d2009d53db74763b55
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-09-12 18:47:06 +0200

    Update _prod compilation script. Update Symfony requirements. Update Sensio distribution bundle to version 4.0.9.

--- Web/composer.prod.sh
+++ Web/composer.prod.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-export SYMFONY_ENV=prod && sudo rm -rf app/cache/prod && sudo rm -rf app/cache/dev sudo rm -rf app/logs/prod.log && sudo rm -rf app/logs/dev.log && sudo rm -rf vendor && sudo rm -rf web/bundles && sudo rm -rf web/resources/css && sudo rm -rf web/resources/js && php composer.phar install --no-dev --optimize-autoloader && php app/console cache:clear --env=prod --no-debug && php app/console assetic:dump --env=prod --no-debug && chown www-data.www-data * -R && chown www-data.www-data .* -R
+export SYMFONY_ENV=prod &&
+sudo rm -rf app/cache/prod &&
+sudo rm -rf app/cache/dev &&
+sudo rm -rf app/logs/prod.log &&
+sudo rm -rf app/logs/dev.log &&
+sudo rm -rf vendor &&
+sudo rm -rf web/bundles &&
+sudo rm -rf web/resources/css &&
+sudo rm -rf web/resources/js &&
+php composer.phar install --no-dev --optimize-autoloader &&
+php app/console cache:clear --env=prod --no-debug &&
+php app/console assetic:dump --env=prod --no-debug &&
+chown www-data.www-data * -R &&
