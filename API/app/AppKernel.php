a6cf2d08922bba87af0f615d1a209306742a0abd
GrappBox (server) <grappbox@gmail.com>
2016-09-12 15:56:10 +0000

    Enable _prod compilation. - Fix missing Sensio-generator bundle - Remove league/ vendor from composer automatic install (keeping wieser_m data)

--- API/app/AppKernel.php
+++ API/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+ 	    new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new SQLBundle\SQLBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
@@ -26,7 +27,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
