5c9ea2ae64335c5aaeda6d968f1aa14a329c04d1
GrappBox (server) <grappbox@gmail.com>
2016-09-12 17:13:32 +0000

    Add _prod compilation script.

--- API/composer.prod.sh
+++ API/composer.prod.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 export SYMFONY_ENV=prod &&
+sudo service apache2 stop &&
 sudo rm -rf app/cache/prod &&
 sudo rm -rf app/cache/dev &&
 sudo rm -rf app/logs/prod.log &&
 sudo rm -rf app/logs/dev.log &&
-sudo rm -rf vendor/* &&
-sudo git checkout vendor/ &&
+sudo find vendor/* \! -name 'league' -and \! -name 'sabre' -delete &&
 php composer.phar install --no-dev --optimize-autoloader &&
 php app/console cache:clear --env=prod --no-debug &&
+sudo git checkout vendor/ &&
 chown www-data.www-data * -R &&
-chown www-data.www-data .* -R
+chown www-data.www-data .* -R &&
+sudo service apache2 start
