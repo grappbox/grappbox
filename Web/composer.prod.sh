b07203c8c24db269dc982abff62b3b1376ebe8e8
GrappBox (server) <grappbox@gmail.com>
2016-09-12 16:55:56 +0000

    Fix _prod compilation script (UNIX side).

--- Web/composer.prod.sh
+++ Web/composer.prod.sh
@@ -13,3 +13,4 @@ php composer.phar install --no-dev --optimize-autoloader &&
 php app/console cache:clear --env=prod --no-debug &&
 php app/console assetic:dump --env=prod --no-debug &&
 chown www-data.www-data * -R &&
+chown www-data.www-data .* -R
