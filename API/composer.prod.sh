3e7a2116508a792d50dbf4444a7431f5b406f8c3
GrappBox (server) <grappbox@gmail.com>
2016-09-15 14:27:19 +0000

    Update deployment scripts (2/2). - Add "version" variable

--- API/composer.prod.sh
+++ API/composer.prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+VERSION=0.3
+
 export SYMFONY_ENV=prod &&
 sudo rm -rf app/cache/prod &&
 sudo rm -rf app/cache/dev &&
@@ -11,4 +13,4 @@ sudo php app/console cache:clear --env=prod --no-debug &&
 sudo git checkout vendor/ &&
 sudo chown www-data.www-data * -R &&
 sudo chown www-data.www-data .* -R &&
-sudo apidoc -f .php -i /var/www/api/versions/0.3/Grappbox/API/src/SQLBundle/Controller/ -o /var/www/doc/0.3/ --silent
+sudo apidoc -f .php -i /var/www/api/versions/$VERSION/Grappbox/API/src/SQLBundle/Controller/ -o /var/www/doc/$VERSION/ --silent
