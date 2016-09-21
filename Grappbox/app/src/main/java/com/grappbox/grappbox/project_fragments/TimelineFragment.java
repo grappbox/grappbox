327920ef23dbd75ff1ad349919bb874a1ac0862d
tan-frederic <frederic.tan@epitech.eu>
2016-09-21 22:09:22 +0200

    begening of the timeline page. View pager add

--- Grappbox/app/src/main/java/com/grappbox/grappbox/project_fragments/TimelineFragment.java
+++ Grappbox/app/src/main/java/com/grappbox/grappbox/project_fragments/TimelineFragment.java
@@ -3,17 +3,22 @@ package com.grappbox.grappbox.project_fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.grappbox.grappbox.R;
+import com.grappbox.grappbox.adapter.BugListFragmentStatePagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TimelineFragment extends Fragment {
 
+    private FragmentStatePagerAdapter mPagesAdapter;
+    private ViewPager mViewPager;
 
     public TimelineFragment() {
         // Required empty public constructor
@@ -24,7 +29,14 @@ public class TimelineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false);
+        View v = inflater.inflate(R.layout.fragment_bugtracker, container, false);
+        mViewPager = (ViewPager) v.findViewById(R.id.viewPager);
+
+        mPagesAdapter = new BugListFragmentStatePagerAdapter(getActivity(), getActivity().getSupportFragmentManager());
+        mViewPager.setAdapter(mPagesAdapter);
+        mViewPager.setOffscreenPageLimit(2);
+
+        return v;
     }
 
 }
