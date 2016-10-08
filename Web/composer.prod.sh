51e6a004a23364ec491d83c85928d8b2d4f2f63f
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-10-08 16:43:58 +0200

    Update _prod deployment script. - Add browscap database update on deploy

--- Web/composer.prod.sh
+++ Web/composer.prod.sh
@@ -12,6 +12,7 @@ sudo rm -rf web/resources/js/* &&
 sudo git checkout web/resources/css/ &&
 sudo git checkout web/resources/js/ &&
 php composer.phar install --no-dev --optimize-autoloader &&
+vendor/bin/browscap-php browscap:update &&
 php app/console cache:clear --env=prod --no-debug &&
 php app/console assetic:dump --env=prod --no-debug &&
 sudo chown www-data.www-data * -R &&
