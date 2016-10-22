175309d68f310b193418f1f138e850a4ec669b25
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2016-10-22 17:58:21 +0200

    Add Google Analytics tags. Add social media SEO tags.

---
+++ Web/src/GrappBox/Bundle/AppBundle/Resources/public/js/home/analytics.js
@@ -0,0 +1,17 @@
+/*
+* This file is subject to the terms and conditions defined in
+* file 'LICENSE.txt', which is part of the GRAPPBOX source code package.
+* COPYRIGHT GRAPPBOX. ALL RIGHTS RESERVED.
+*/
+
+/* =================================================== */
+/* ==================== ANALYTICS ==================== */
+/* =================================================== */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-86114028-1', 'auto');
+ga('send', 'pageview');
