8dcb93c8d97cf2d72bf8c848f17ba96ec3fa35a9
mougen-v <mougen_v@epitech.eu>
2016-11-08 23:35:44 +0100

    test config travis + add test for register

---
+++ API/src/SQLBundle/Tests/Controller/AccountAdministrationControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SQLBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AccountAdministrationControllerTest extends WebTestCase
+{
+
+    public function testRegister()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+        	'POST',
+        	'http://127.0.0.1:8000/account/register',
+        	array(),
+        	array(),
+        	array('CONTENT_TYPE' => 'application/json'),
+        	'{ "data": { "firstname": "john", "lastname": "doe", "password": "toto", "email": "yolo@toto.com", "is_client": false, "mac": "XXXX", "flag": "web", "device_name": "yolo" } }'
+        );
+
+        // Assert a specific 200 status code
+		$this->assertEquals(
+		    201, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
+		    $client->getResponse()->getStatusCode()
+		);
+
+		$data = json_decode($client->getResponse()->getContent(), true);
+		$data = $data['data'];
+		$this->assertEquals("john", $data->firstname);
+		$this->assertEquals("doe", $data->lastname);
+		$this->assertEquals("yolo@toto.com", $data->email);
+    }
+}
