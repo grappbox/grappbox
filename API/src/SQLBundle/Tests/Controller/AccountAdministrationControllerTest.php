3a9dfde80a0ff1cff7281eb288778b9dd8491e44
mougen-v <mougen_v@epitech.eu>
2016-11-10 16:16:34 +0100

    Add login et logout test

--- API/src/SQLBundle/Tests/Controller/AccountAdministrationControllerTest.php
+++ API/src/SQLBundle/Tests/Controller/AccountAdministrationControllerTest.php
@@ -47,4 +47,66 @@ class AccountAdministrationControllerTest extends WebTestCase
             $client->getResponse()->getStatusCode()
         );
     }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+            'POST',
+            '/account/login',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{ "data": { "password": "toto", "login": "yolo@toto.com", "mac": "XXXX", "flag": "web", "device_name": "yolo" } }'
+        );
+
+        // Assert a specific 200 status code
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $data = $data['data'];
+        $this->assertEquals("john", $data['firstname']);
+        $this->assertEquals("doe", $data['lastname']);
+        $this->assertEquals("yolo@toto.com", $data['email']);
+        $_ENV['TOKEN'] = $data['token'];
+    }
+
+    public function testLogout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+            'GET',
+            '/account/logout',
+            array(),
+            array(),
+            array('HTTP_Authorization' => $_ENV['TOKEN'])
+        );
+
+        // Assert a specific 200 status code
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $crawler = $client->request(
+            'POST',
+            '/account/login',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{ "data": { "password": "toto", "login": "yolo@toto.com", "mac": "XXXX", "flag": "web", "device_name": "yolo" } }'
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $data = $data['data'];
+        $this->assertEquals("john", $data['firstname']);
+        $this->assertEquals("doe", $data['lastname']);
+        $this->assertEquals("yolo@toto.com", $data['email']);
+        $_ENV["TOKEN"] = $data['token'];
+    }
 }
