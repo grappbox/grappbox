7c1d4579a239dd09b5d394ddba39787b99d210b0
tan-frederic <frederic.tan@epitech.eu>
2016-11-10 11:30:42 +0100

    Remove speed and beta.

---
+++ Grappbox/app/src/main/java/com/grappbox/grappbox/model/EventDateFormatModel.java
@@ -0,0 +1,18 @@
+package com.grappbox.grappbox.model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by tan_f on 10/11/2016.
+ */
+
+public class EventDateFormatModel {
+
+    Date _date;
+
+    EventDateFormatModel() {
+
+    }
+
+}
