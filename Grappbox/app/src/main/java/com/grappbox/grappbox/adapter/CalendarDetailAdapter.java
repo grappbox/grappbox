24d663d9d5206f327a21b7fe27775339ff841711
tan-frederic <frederic.tan@epitech.eu>
2016-12-01 19:02:16 +0100

    Start to fill the detail view of the event detail.

--- Grappbox/app/src/main/java/com/grappbox/grappbox/adapter/CalendarDetailAdapter.java
+++ Grappbox/app/src/main/java/com/grappbox/grappbox/adapter/CalendarDetailAdapter.java
@@ -1,8 +1,130 @@
 package com.grappbox.grappbox.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.grappbox.grappbox.R;
+import com.grappbox.grappbox.model.CalendarEventModel;
+import com.grappbox.grappbox.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tan_f on 01/12/2016.
  */
 
-public class CalendarDetailAdapter {
+public class CalendarDetailAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_DATE = 0;
+    private static final int TYPE_SUBTITLE = 1;
+    private static final int TYPE_PARTICIPANT = 2;
+
+    private List<UserModel> mParticipant;
+
+    private CalendarEventModel mModel;
+    private LayoutInflater mInflater;
+    private Context mContext;
+
+    public CalendarDetailAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mParticipant = new ArrayList<>();
+    }
+
+    public void setEventModel(CalendarEventModel model) {
+        mModel = model;
+        mParticipant = model._user;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case TYPE_SUBTITLE:
+                return new SubtitleHolder(mInflater.inflate(R.layout.list_item_custom_title, parent, false));
+
+            case TYPE_DATE:
+                return new DateHolder(mInflater.inflate(R.layout.list_item_calendar_date, parent, false));
+
+            case TYPE_PARTICIPANT:
+                return new ParticipantHolder(mInflater.inflate(R.layout.list_item_calendar_participant, parent, false));
+
+            default:
+                throw new IllegalArgumentException("Bad viewType : " + viewType);
+        }
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position, List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (getItemViewType(position)) {
+            case TYPE_SUBTITLE:
+                break;
+
+            case TYPE_DATE:
+                break;
+
+            case TYPE_PARTICIPANT:
+                break;
+
+            default:
+                throw new IllegalArgumentException("Bad viewType" + getItemViewType(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    private static class DateHolder extends RecyclerView.ViewHolder {
+
+        public TextView mBeginDate;
+        public TextView mEndDate;
+
+        public DateHolder(View itemView) {
+            super(itemView);
+            mBeginDate = (TextView)itemView.findViewById(R.id.event_begin);
+            mEndDate = (TextView)itemView.findViewById(R.id.event_end);
+        }
+    }
+
+    private static class ParticipantHolder extends RecyclerView.ViewHolder {
+
+        public ImageView mAvatar;
+        public TextView mUsername;
+
+
+        public ParticipantHolder(View itemView) {
+            super(itemView);
+            mUsername = (TextView) itemView.findViewById(R.id.username);
+            mAvatar = (ImageView) itemView.findViewById(R.id.avatar);
+        }
+    }
+
+    private static class  SubtitleHolder extends RecyclerView.ViewHolder {
+
+        public TextView description;
+
+        public SubtitleHolder(View itemView) {
+            super(itemView);
+            description = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
 }
--- Grappbox/app/src/main/java/com/grappbox/grappbox/calendar_fragment/EventDetailsFragment.java
+++ Grappbox/app/src/main/java/com/grappbox/grappbox/calendar_fragment/EventDetailsFragment.java
@@ -6,11 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.grappbox.grappbox.R;
+import com.grappbox.grappbox.model.CalendarEventModel;
 
 /**
  * Created by tan_f on 16/11/2016.
@@ -18,11 +20,16 @@ import com.grappbox.grappbox.R;
 
 public class EventDetailsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private RecyclerView mRecycler;
+    private CalendarEventModel mModel;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        mModel = getActivity().getIntent().getParcelableExtra(EventDetailsActivity.EXTRA_CALENDAR_EVENT_MODEL);
         View v = inflater.inflate(R.layout.fragment_calendar_event_details, container, false);
-
+        mRecycler = (RecyclerView)v.findViewById(R.id.scrollable_content);
 
         return v;
 
