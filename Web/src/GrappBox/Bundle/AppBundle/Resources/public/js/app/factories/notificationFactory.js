92ed378193675fdaf01c757528711840e22d05b5
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2017-01-15 01:43:47 +0100

    Add new Cloud page (1/2). - Add new Cloud design - Add new controller structure and indentation

--- Web/src/GrappBox/Bundle/AppBundle/Resources/public/js/app/factories/notificationFactory.js
+++ Web/src/GrappBox/Bundle/AppBundle/Resources/public/js/app/factories/notificationFactory.js
@@ -18,22 +18,37 @@ app.factory("notificationFactory", ['Notification', function(Notification) {
       Notification.clearAll();
     },
 
+    // "Loading" routine
+    // Operation in progress
+    loading: function(message) {
+      if (message)
+        Notification.success({ message: message + "<i class=\"material-icons\" aria-hidden=\"true\">more_vert</i>", delay: 1000 });
+      else
+        Notification.success({ message: "Loading...<i class=\"material-icons\" aria-hidden=\"true\">more_vert</i>", delay: 1000 });
+    },
+
+    // "Information" routine
+    // Operation in progress
+    info: function(message) {
+      Notification.success({ message: message + "<i class=\"material-icons\" aria-hidden=\"true\">help</i>", delay: 2000 });
+    },
+
     // "Success" routine
     // Operation validated
     success: function(message) {
-      Notification.success({ message: message + "<i class=\"material-icons\">check</i>", delay: 1000 });
+      Notification.success({ message: message + "<i class=\"material-icons\" aria-hidden=\"true\">check</i>", delay: 1000 });
     },
     
     // "Warning" routine
     // Operation failed, or succeed but with non-blocking errors
     warning: function(message) {
-      Notification.warning({ message: message + "<i class=\"material-icons\">warning</i>", delay: 2000 });
+      Notification.warning({ message: message + "<i class=\"material-icons\" aria-hidden=\"true\">warning</i>", delay: 2000 });
     },
 
     // "Error" routine
     // Operation critically failed    
     error: function() {
-      Notification.error({ message: "Someting is wrong with GrappBox. Please give us a minute, and try again.<i class=\"material-icons\">clear</i>", delay: 3000 });
+      Notification.error({ message: "Someting is wrong with GrappBox. Please give us a minute, and try again.<i class=\"material-icons\" aria-hidden=\"true\">clear</i>", delay: 3000 });
     },
   };
 
